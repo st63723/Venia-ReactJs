c6452ffd26ad7e20fd91ab279cb9aea5588409f3
Sadanandam Tallapelly <sadanandam.tallapelly@zensar.com>
2022-06-23 11:22:32 +0530

    routers changed

--- src/Router.js
+++ src/Router.js
@@ -14,6 +14,7 @@ import Footer from './components/Footer/Footer';
   <BrowserRouter>
     <App>
     <Routes>
+    <Route path="/" element={ <Home/> } />
           <Route path="/veniareactjs" element={ <Home/> } />
           <Route path="/Home" element={ <Home/> } />
           <Route path="/products/:productId" element={ <ProductDetail/>} />
